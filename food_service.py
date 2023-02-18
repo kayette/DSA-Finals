d10170a1e6e2af9c1d92045456e1a897459d153e
Vienne Kayette Javier <viennekayetteljavier@iskolarngbayan.pup.edu.ph>
2023-02-18 19:38:36 +0800

    Added voice command

--- food_service.py
+++ food_service.py
@@ -1,3 +1,5 @@
+from tkinter import *
+import speech_recognition as sr
 orderQueue = []
 
 # first operation
@@ -23,22 +25,34 @@ def serve_queue():
     (customer_name, order) = orderQueue.pop(0)
     print(f"\n===================================================\n(DISPLAY QUEUE)\n\nNow Serving:\n{customer_name} - {order}\n")
 
+print("\n===================================================\n(MENU)\nWhat would you like to do?\n")
+print("1. I'm ready to order!\n2. Is my order ready?\n3. Order up!\n4. Leave")
+
 # loop
 while True:
-    print("\n===================================================\n(MENU)\nWhat would you like to do?\n")
-    print("1. I'm ready to order!\n2. Is my order ready?\n3. Order up!\n4. Leave")
 
-    user_input = input("\nEnter your destination: ")
-    if user_input == "1":
-        customer_name = input("\n===================================================\n(ORDER)\nGreat! You are now making an order.\nPlease enter your name: ")
-        order = input("\nEnter your order: ")
-        take_order(customer_name, order)
-    elif user_input == "2":
-        show_queue()
-    elif user_input == "3":
-        serve_queue()
-    elif user_input == "4":
-        print("\nThank you for using Food Kiosk! Please come again.\n")
-        break
-    else: 
-        print("===================================================\nInvalid Input\nPlease Try Again.\n")
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source,duration=1)
+        print("\nListening...")
+        audio = r.listen(source)
+        try:
+            print("\nRecognizing...")
+            text = r.recognize_google(audio)
+            print("\nYou said: " + text)
+            if text == "one":
+                customer_name = input("\n===================================================\n(ORDER)\nGreat! You are now making an order.\nPlease enter your name: ")
+                order = input("\nEnter your order: ")
+                take_order(customer_name, order)
+            elif text == "two":
+                show_queue()
+            elif text == "three":
+                serve_queue()
+            elif text == "four":
+                print("\nThank you for using Food Kiosk! Please come again.\n")
+                break
+        except Exception as e:
+            print(e)
+            answer = "\nSorry, I did not get that. Come again?"
+            print(answer)
